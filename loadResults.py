d4a3ee9b281b43ec63daa1d2ce349c92dbe9aa26
michelebanfi <michi.banfi01@gmail.com>
2025-12-02 09:47:02 +0100

    updates in the visualization

---
+++ loadResults.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+result = np.load("data/LERS.npz", allow_pickle=True)
+
+print(dict(result))
+
+PER = result["physicalErrorRates"]
+print(PER)
+
+res = result["results"].item() # idk why .item() is needed, soooo python of him to require that
+print(res)
+
+plt.figure(figsize=(10,6))
+for item in res:
+    plt.plot(PER, res[item], label=item, marker='o')
+    plt.xscale('log')
+    # plt.yscale('log')
+    plt.legend()
+    plt.grid(True)
+    print(res[item])
+plt.savefig("media/LERSReloaded.png")
