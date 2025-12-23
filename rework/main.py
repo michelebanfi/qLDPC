ba97ae2066eab575e7d796a41e09685298307649
michelebanfi <michi.banfi01@gmail.com>
2025-12-23 14:14:40 +0100

    Degeneracies and weight histogram

--- rework/main.py
+++ rework/main.py
@@ -38,6 +38,8 @@ experiment = [
     },
 ]
 
+experiment_dict = {exp["name"]: exp for exp in experiment}
+
 trials = 10
 
 np.random.seed(0)
@@ -56,7 +58,12 @@ for exp in experiment:
 
         logicalError = 0
         OSD_invocations = 0
-        weights_found = []
+        degenerateErrors = 0
+        weights_found_BP = []
+        weights_found_OSD = []
+        
+        weights_found_BP_error = []
+        weights_found_OSD_error = []
         
 
         for _ in tqdm.tqdm(range(trials), desc=f"Code {code_name}, p={errorRate}"):
@@ -71,31 +78,50 @@ for exp in experiment:
             if not isSyndromeFound:
                 detection = performOSD_enhanced(code, syndrome, llrs, detection, order=2)
                 OSD_invocations += 1
-
-            if np.any(detection != error):
-                logicalError += 1
                 
             residual = (detection + error) % 2
             syndromeLogic = (Lx @ residual) % 2
+            
+            osd_syndrome_check = (detection @ code.T) % 2
+            is_valid_osd = np.array_equal(osd_syndrome_check, syndrome)
+            
+            if is_valid_osd and not np.any(syndromeLogic) and (np.array_equal(detection, error) == False):
+                degenerateErrors += 1
+                
+            if np.any(syndromeLogic):
+                logicalError += 1
+                if not isSyndromeFound:
+                    weights_found_OSD_error.append(np.sum(residual))
+                if isSyndromeFound:
+                    weights_found_BP_error.append(np.sum(residual))
+            
             if not np.any(syndromeLogic):
                 if not np.array_equal(detection, error):
                     w = np.sum(residual)
-                    weights_found.append(w)
+                    if not isSyndromeFound: weights_found_OSD.append(w)
+                    if isSyndromeFound: weights_found_BP.append(w)
 
         logicalErrorRate = logicalError / trials
         OSD_invocationRate = OSD_invocations / trials
+        degenerateErrorRate = degenerateErrors / trials
         results[code_name][errorRate] = {
             "logical": logicalErrorRate,
             "osd": OSD_invocationRate,
-            "weights_found": weights_found,
+            "degeneracies": degenerateErrorRate,
+            "weights_found_BP": weights_found_BP,
+            "weights_found_OSD": weights_found_OSD,
+            "weights_found_BP_error": weights_found_BP_error,
+            "weights_found_OSD_error": weights_found_OSD_error,
         }
         print(
             f"Code {code_name}, p={errorRate}, Logical Error Rate: {logicalErrorRate}, OSD Invocation Rate: {OSD_invocationRate}"
         )
+        
+np.savez("rework/simulation_results.npz", results=results)
 
 colors = ["2E72AE", "64B791", "DBA142", "000000", "E17792"]
 
-fig, axes = plt.subplots(2, 1, figsize=(6, 8), sharex=True)
+fig, axes = plt.subplots(3, 1, figsize=(6, 8), sharex=True)
 for (code_name, code_results), color in zip(results.items(), colors):
     x = list(code_results.keys())
     axes[0].semilogy(
@@ -112,29 +138,76 @@ for (code_name, code_results), color in zip(results.items(), colors):
         label=f"Code {code_name}",
         color=f"#{color}",
     )
+    axes[2].plot(
+        x,
+        [v["degeneracies"] for v in code_results.values()],
+        marker="s",
+        label=f"Code {code_name}",
+        color=f"#{color}",
+    )
 
 axes[0].set_ylabel("Logical Error Rate")
 axes[0].grid(True, which="both", ls="--")
 axes[0].legend()
 
-axes[1].set_xlabel("Physical Error Rate")
 axes[1].set_ylabel("OSD Invocation Rate")
 axes[1].grid(True, which="both", ls="--")
 axes[1].legend()
 
+axes[2].set_xlabel("Physical Error Rate")
+axes[2].set_ylabel("Degenerate Errors Rate")
+axes[2].grid(True, which="both", ls="--")
+axes[2].legend()
+
 plt.tight_layout()
 plt.savefig("rework/logical_error_rates.png", dpi=300)
 
-fig2, axes2 = plt.subplots(1, 5, figsize=(15, 4))
+fig2, axes2 = plt.subplots(2, 5, figsize=(15, 8))
 for i, (code_name, code_results) in enumerate(results.items()):
-    weights = []
+    weights_BP = []
+    weights_OSD = []
     for res in code_results.values():
-        weights.extend(res["weights_found"])
-        
-    if weights:
-        axes2[i].hist(weights, bins=range(min(weights), max(weights)+2), color=f"#{colors[i]}", alpha=0.7)
-    axes2[i].set_title(f"Code {code_name}")
-    axes2[i].set_xlabel("Weight")
-    axes2[i].set_ylabel("Frequency")
+        weights_BP.extend(res["weights_found_BP"])
+        weights_OSD.extend(res["weights_found_OSD"])
+    
+    if weights_BP:
+        axes2[0, i].hist(weights_BP, bins=np.arange(0, 30)-0.5, color=f"#{colors[i]}", alpha=0.7)
+    axes2[0, i].axvline(x=experiment_dict[code_name]['distance'], color='red', linestyle='dashed', label='Code Distance')
+    axes2[0, i].set_title(f"Code {code_name} (BP)")
+    axes2[0, i].set_xlabel("Weight")
+    axes2[0, i].set_ylabel("Frequency")
+    
+    if weights_OSD:
+        axes2[1, i].hist(weights_OSD, bins=np.arange(0, 30)-0.5, color=f"#{colors[i]}", alpha=0.7)
+    axes2[1, i].axvline(x=experiment_dict[code_name]['distance'], color='red', linestyle='dashed', label='Code Distance')
+    axes2[1, i].set_title(f"Code {code_name} (OSD)")
+    axes2[1, i].set_xlabel("Weight")
+    axes2[1, i].set_ylabel("Frequency")
+
 plt.tight_layout()
 plt.savefig("rework/weights_histograms.png", dpi=300)
+
+fig2, axes2 = plt.subplots(2, 5, figsize=(15, 8))
+for i, (code_name, code_results) in enumerate(results.items()):
+    weights_BP = []
+    weights_OSD = []
+    for res in code_results.values():
+        weights_BP.extend(res["weights_found_BP_error"])
+        weights_OSD.extend(res["weights_found_OSD_error"])
+    
+    if weights_BP:
+        axes2[0, i].hist(weights_BP, bins=np.arange(0, 30)-0.5, color=f"#{colors[i]}", alpha=0.7)
+    axes2[0, i].axvline(x=experiment_dict[code_name]['distance'], color='red', linestyle='dashed', label='Code Distance')
+    axes2[0, i].set_title(f"Code {code_name} (BP)")
+    axes2[0, i].set_xlabel("Weight")
+    axes2[0, i].set_ylabel("Frequency")
+    
+    if weights_OSD:
+        axes2[1, i].hist(weights_OSD, bins=np.arange(0, 30)-0.5, color=f"#{colors[i]}", alpha=0.7)
+    axes2[1, i].axvline(x=experiment_dict[code_name]['distance'], color='red', linestyle='dashed', label='Code Distance')
+    axes2[1, i].set_title(f"Code {code_name} (OSD)")
+    axes2[1, i].set_xlabel("Weight")
+    axes2[1, i].set_ylabel("Frequency")
+
+plt.tight_layout()
+plt.savefig("rework/weights_histograms_ERROR.png", dpi=300)
